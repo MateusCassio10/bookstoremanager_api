947242946b6c9d0f264e8a2ddf3a2773bcde84e8
MateusCassio10 <mateuscassio07@gmail.com>
2022-10-24 14:30:46 -0300

    First commit

---
+++ src/test/java/com/mateus/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
