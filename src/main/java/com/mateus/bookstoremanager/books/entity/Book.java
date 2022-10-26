f7be197db3b8fd437e9f33269ea72284c24bcc37
MateusCassio10 <mateuscassio07@gmail.com>
2022-10-26 12:10:30 -0300

    Created model mappings

---
+++ src/main/java/com/mateus/bookstoremanager/books/entity/Book.java
@@ -0,0 +1,38 @@
+package com.mateus.bookstoremanager.books.entity;
+
+import com.mateus.bookstoremanager.author.entity.Author;
+import com.mateus.bookstoremanager.publishers.entity.Publisher;
+import com.mateus.bookstoremanager.users.entity.User;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(columnDefinition = "integer default 0")
+    private int pages;
+
+    @Column(columnDefinition = "integer default 0")
+    private int chapters;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Author author;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private Publisher publisher;
+
+    @ManyToOne(cascade = {CascadeType.MERGE})
+    private User user;
+}
